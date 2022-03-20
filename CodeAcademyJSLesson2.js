b6fbd449e02ee5984963430f50692cdedd876673
Charles Barnes <onthisrock97@gmail.com>
2022-03-19 23:31:27 -0400

    Variables

---
+++ CodeAcademyJSLesson2.js
@@ -0,0 +1,17 @@
+// Code Academy JavaScript Lesson 2: Variables
+//Part 1 Creating a Variable: var
+var favoriteFood = 'pizza'
+var numOfSlices = 8
+console.log(favoriteFood)
+console.log(numOfSlices)
+
+//Part 2 Create a Variable: let
+
+//Part 3
+//Part 4
+//Part 5
+//Part 6
+//Part 7
+//Part 8
+//Part 9
+
