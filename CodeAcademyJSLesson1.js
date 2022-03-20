a342576afcd8af0c77b15e029752f4409b1c3561
Charles Barnes <onthisrock97@gmail.com>
2022-03-19 23:21:26 -0400

    Code Academy's Introduction to JavaScript Lesson.

---
+++ CodeAcademyJSLesson1.js
@@ -0,0 +1,47 @@
+//Code Academy JavaScript Lesson 1
+
+//Part 1 Introduction
+//console.log(24);
+//console.log(104)
+
+//Part 2 Comments
+// Opening line
+console.log('It was love at first sight.');
+/*
+console.log('The first time Yossarian saw the chaplain he fell madly in love with him.');
+console.log('Yossarian was in the hospital with a pain in his liver that fell just short of being jaundice.');
+console.log('The doctors were puzzled by the fact that it wasn\'t quite jaundice.');
+console.log('If it became jaundice they could treat it.');
+console.log('If it didn\'t become jaundice and went away they could discharge him.');
+console.log('But this just being short of jaundice all the time confused them.');
+*/
+//Part 3 Data Types
+console.log('JavaScript')
+console.log(2011)
+console.log('Woohoo! I love to code! #codecademy')
+console.log(20.49)
+
+//Part 4 Arithmetic Operators
+console.log(3.5+24) /* The answer is how old I will be when people will be sent to live on Mars. */
+console.log(2022-1969) /* The answer is how many years it has been since the 1969 Moon Landing. */
+console.log(65/240)
+console.log(0.2708*100) /* This is the % of the sun that is made of Helium */
+
+//Part 5 String Concatenation
+console.log('Hello'+'World')
+console.log('Hello'+' '+'World')
+
+//Part 6 Properties
+console.log('Teaching the world how to code'.length)
+
+//Part 7 Methods
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+
+//Part 8 Built-in Objects
+console.log(Math.floor(Math.random()*100))
+console.log(Math.ceil(43.8))
+console.log(Number.isInteger(2017))
